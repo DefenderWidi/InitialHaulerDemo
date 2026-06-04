1466a5f2622bec3b3e9e194450a5d353ddcc1e20
defenderwidi <defenderwidi17@gmail.com>
2026-06-04 11:41:40 +0800

    Fix unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react";
 
 type Status = "UNDERLOAD" | "IDEAL" | "OVERLOAD"
 type ValidationStatus = "PENDING" | "VALID" | "INVALID"
